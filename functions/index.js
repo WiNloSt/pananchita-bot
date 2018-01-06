afefef241b288eced031235fdbf240b195f7bcea
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-01-06 13:09:15 +0700

    :sparkles: reply with awesome message

--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,26 @@ exports.lineWebhook = functions.https.onRequest((request, response) => {
 })
 
 const eventHandler = event => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (
+    event.type !== 'message' ||
+    event.message.type !== 'text' ||
+    !event.message.text.match('เปรี้ยง')
+  ) {
     return null
   }
 
-  return replyMessage(event.replyToken, 'Hello from the other side')
+  const message = randomMessage()
+
+  return replyMessage(event.replyToken, message)
 }
+
+const randomMessage = () => MESSAGES[parseInt(Math.random() * 6)]
+
+const MESSAGES = [
+  '💄 No.1 Lucien แปลว่า พบเจอแสงสว่าง',
+  '💄 No.2 Miguel มิเกล แปลว่า ดุจเทพเจ้า',
+  '💄 No.3 Tadeo ทาดีโอ้ แปลว่า มีแต่คนยกย่อง',
+  '💄 No.4 Ecio อี ซิ โอ้ ภาษาละติน ถูกสิงสู่สิ่งดีๆ โดยพลังอันเข้มแข็ง',
+  '💄 No.5 Racknar แร็คน่า พลังอันแข็งแกร่ง',
+  '💄 No.6 Olivia โอลิเวีย สัญลักษณ์แห่งความสุข'
+]
